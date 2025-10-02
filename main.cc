1b9943177a78b7a9baf743a592d5ab0c745c83d5
A.Keshavarz <alireza.keshavarz.d@gmail.com>
2025-10-02 21:02:20 +0330

    add cxxopts library for parsing cmd options

--- main.cc
+++ main.cc
@@ -2,11 +2,23 @@
 #include <iostream>
 
 #include "fmt/core.h"
+#include "cxxopts.hpp"
 
 
 int main(int argc, char** argv) {
-    std::cout << std::filesystem::current_path() << std::endl;
     fmt::print("Hello World\n");
+    std::cout << std::filesystem::current_path() << std::endl;
+
+    cxxopts::Options options("SourceTokenizer", "One line description of MyProgram");
+
+    options.add_options()
+      ("d,debug", "Enable debugging") // a bool parameter
+      ("i,integer", "Int param", cxxopts::value<int>())
+      ("f,file", "File name", cxxopts::value<std::string>())
+      ("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
+      ;
+
+    auto result = options.parse(argc, argv);
 
     return 0;
 }
